bd71469a3fa4dc410d3288777f1a7cc42259bb61
recall704 <tk657309822@gmail.com>
2025-03-17 21:37:17 +0800

    update prepare image shell

--- deploy/kubernetes/csi-rclone-chart/prepare_img.sh
+++ deploy/kubernetes/csi-rclone-chart/prepare_img.sh
@@ -1,21 +1,40 @@
 #!/bin/bash
 
 # 准备一个本地镜像仓库
-LOCAL_REGISTRY="registry.cn-shanghai.aliyuncs.com/tk-mirrors"
+# 支持用户输入 LOCAL_REGISTRY
 
-docker pull registry.k8s.io/sig-storage/csi-provisioner:v5.0.2
-docker pull k8s.gcr.io/sig-storage/csi-attacher:v3.4.0
-docker pull k8s.gcr.io/sig-storage/csi-node-driver-registrar:v2.4.0
-docker pull wunderio/csi-rclone:v3.0.0
+# 使用方法
+# ./prepare_img.sh [registry]
+# 例如: ./prepare_img.sh registry.example.com/my-repo
 
-# tag
-docker tag registry.k8s.io/sig-storage/csi-provisioner:v5.0.2 $LOCAL_REGISTRY/csi-provisioner:v5.0.2
-docker tag k8s.gcr.io/sig-storage/csi-attacher:v3.4.0 $LOCAL_REGISTRY/csi-attacher:v3.4.0
-docker tag k8s.gcr.io/sig-storage/csi-node-driver-registrar:v2.4.0 $LOCAL_REGISTRY/csi-node-driver-registrar:v2.4.0
-docker tag wunderio/csi-rclone:v3.0.0 $LOCAL_REGISTRY/wunderio-csi-rclone:v3.0.0
+DEFAULT_REGISTRY="local.harbor.io/release2"
 
-# push
-docker push $LOCAL_REGISTRY/csi-provisioner:v5.0.2
-docker push $LOCAL_REGISTRY/csi-attacher:v3.4.0
-docker push $LOCAL_REGISTRY/csi-node-driver-registrar:v2.4.0
-docker push $LOCAL_REGISTRY/wunderio-csi-rclone:v3.0.0
+# 如果用户没有输入值，则使用默认值
+if [ -z "$1" ]; then
+  LOCAL_REGISTRY="$DEFAULT_REGISTRY"
+  echo "Using default registry: $LOCAL_REGISTRY"
+else
+  LOCAL_REGISTRY="$1"
+  echo "Using custom registry: $LOCAL_REGISTRY"
+fi
+
+
+echo "docker pull registry.cn-shanghai.aliyuncs.com/tk-mirrors/csi-provisioner:v5.0.2"
+echo "docker pull registry.cn-shanghai.aliyuncs.com/tk-mirrors/csi-attacher:v3.4.0"
+echo "docker pull registry.cn-shanghai.aliyuncs.com/tk-mirrors/csi-node-driver-registrar:v2.4.0"
+echo "docker pull registry.cn-shanghai.aliyuncs.com/tk-mirrors/wunderio-csi-rclone:v3.0.0"
+
+# Print tag commands
+echo "docker tag registry.cn-shanghai.aliyuncs.com/tk-mirrors/csi-provisioner:v5.0.2 $LOCAL_REGISTRY/csi-provisioner:v5.0.2"
+echo "docker tag registry.cn-shanghai.aliyuncs.com/tk-mirrors/csi-attacher:v3.4.0 $LOCAL_REGISTRY/csi-attacher:v3.4.0"
+echo "docker tag registry.cn-shanghai.aliyuncs.com/tk-mirrors/csi-node-driver-registrar:v2.4.0 $LOCAL_REGISTRY/csi-node-driver-registrar:v2.4.0"
+echo "docker tag registry.cn-shanghai.aliyuncs.com/tk-mirrors/wunderio-csi-rclone:v3.0.0 $LOCAL_REGISTRY/wunderio-csi-rclone:v3.0.0"
+
+# Print push commands
+echo "docker push $LOCAL_REGISTRY/csi-provisioner:v5.0.2"
+echo "docker push $LOCAL_REGISTRY/csi-attacher:v3.4.0"
+echo "docker push $LOCAL_REGISTRY/csi-node-driver-registrar:v2.4.0"
+echo "docker push $LOCAL_REGISTRY/wunderio-csi-rclone:v3.0.0"
+
+# Print sed command
+echo "sed -i \"s|registry.cn-shanghai.aliyuncs.com/tk-mirrors|${LOCAL_REGISTRY}|g\" values.yaml"
